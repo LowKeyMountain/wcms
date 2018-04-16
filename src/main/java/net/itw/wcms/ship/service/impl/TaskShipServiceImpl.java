f0fbea9484b167471d809ad2d92dadf42f171bdf
zoush <zoush@zoush-PC>
2018-04-16 19:33:59 +0800

    更新。

--- src/main/java/net/itw/wcms/ship/service/impl/TaskShipServiceImpl.java
+++ src/main/java/net/itw/wcms/ship/service/impl/TaskShipServiceImpl.java
@@ -1075,6 +1075,7 @@ public class TaskShipServiceImpl implements ITaskShipService {
 				
 				datas.add(data);
 			}
+			
 			result.put("msg", msg);
 			result.put("data", datas);
 			result.put("code", isSuccess);
