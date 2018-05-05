444d3d2a423a3812882e21c7c347a1ff30d0a0f2
sparking <sparking@10.211.55.5>
2018-05-05 09:41:26 +0800

    报表完善

--- src/main/webapp/core/report/cabinEffStats.js
+++ src/main/webapp/core/report/cabinEffStats.js
@@ -143,5 +143,5 @@
 	 * 查看船舱卸货详情
 	 */
 	function view_unship(taskId, cabinNo) {
-		window.location.href = BasePath + "/cabin/view?taskId=" + taskId + '&cabinNo=' + cabinNo;
+		window.location.href = BasePath + "/report/getCabinDetail?reportType=5&taskId=" + taskId + '&cabinNo=' + cabinNo;
 	}
--- src/main/webapp/core/report/cabinInfoStats.js
+++ src/main/webapp/core/report/cabinInfoStats.js
@@ -161,5 +161,5 @@
 	 * 查看船舱卸货详情
 	 */
 	function view_unship(taskId, cabinNo) {
-		window.location.href = BasePath + "/cabin/view?taskId=" + taskId + '&cabinNo=' + cabinNo;
+		window.location.href = BasePath + "/report/getCabinDetail?reportType=1&taskId=" + taskId + '&cabinNo=' + cabinNo;
 	}
