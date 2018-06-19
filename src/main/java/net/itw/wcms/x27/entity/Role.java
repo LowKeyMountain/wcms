508fa3d32fa8c53af9c05d709cbdd806b8da712f
zoush <zoush@zoush-PC>
2018-06-19 17:10:20 +0800

    更新。

--- src/main/java/net/itw/wcms/x27/entity/Role.java
+++ src/main/java/net/itw/wcms/x27/entity/Role.java
@@ -23,6 +23,7 @@ import net.itw.wcms.toolkit.hibernate.Entityable;
 @Table(name = "X27_ROLES")
 @Entity
 public class Role implements Entityable {
+
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
